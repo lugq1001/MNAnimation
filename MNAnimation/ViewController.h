d8a93c5fb30513e5bdfec5bc628fec2cec6bee0e
Xiaomuniu <lugq1001@icloud.com>
2014-07-19 10:13:27 +0800

    create project

---
+++ MNAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MNAnimation
+//
+//  Created by 陆广庆 on 14/7/19.
+//  Copyright (c) 2014年 陆广庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MNAnimation/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  MNAnimation
+//
+//  Created by 陆广庆 on 14/7/19.
+//  Copyright (c) 2014年 陆广庆. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
